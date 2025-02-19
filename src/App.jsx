c3c2144ef76f33f3d1e739ee033b00b11b626a7c
Konstantin <106445847+kzaraliev@users.noreply.github.com>
2025-02-19 15:11:16 +0200

    initialize project

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Contact from "./components/contact.jsx";
+import HeroSection from "./components/heroSection.jsx"
+
+import Testemonial from "./components/testimonials.jsx"
+import Stats from "./components/stats.jsx"
+import Pricing from "./components/pricing.jsx"
+import Features from "./components/features.jsx"
+
+function App() {
+
+  return (
+    <>
+    <HeroSection></HeroSection>
+    <Features></Features>
+    <Testemonial></Testemonial>
+    <Stats></Stats>
+    <Pricing></Pricing>
+    <Contact></Contact>
+    </>
+  )
+}
+
+export default App
